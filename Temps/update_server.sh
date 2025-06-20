c75e6a5f84a15e9b849999f56064ddf60c14d3ba
levix1610 <andy4991@gmail.com>
2025-06-19 20:36:06 -0400

    Created lxc update + update check if statement

--- Temps/update_server.sh
+++ Temps/update_server.sh
@@ -5,8 +5,46 @@
 
 # Server Name into Variable
 SERVER="ctus-prod-phi-01.pihole"
+# User Variable
+REMOTE_USER="root"
 
-# SSH command and update commands
-ssh -i .ssh/powers_prod -A $SERVER "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean"
-echo "Update Complete. ($SERVER)"
+# SSH command and update pihole server.
+ssh -i .ssh/powers_prod -A $REMOTE_USER@$SERVER "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean && pihole -up"
+
+# Check the exit status of the ssh command
+if [ $? -eq 0 ]; then
+  echo "Update Complete. ($SERVER)"
+else
+  echo "Update Failed! ($SERVER) - Check for errors above."
+fi
+
 sleep 5
+
+#Set server to next.
+SERVER="ctus-prod-phi-02.pihole"
+# SSH command and update pihole server two.
+ssh -i .ssh/powers_prod -A $REMOTE_USER@$SERVER "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean && pihole -up"
+
+# Check the exit status of the ssh command
+if [ $? -eq 0 ]; then
+  echo "Update Complete. ($SERVER)"
+else
+  echo "Update Failed! ($SERVER) - Check for errors above."
+fi
+
+sleep 5
+
+#Set server to next.
+SERVER="cttk-prod-nxt-01.server"
+# SSH command and update nextcloud LXC.
+ssh -i .ssh/powers_prod -A $REMOTE_USER@$SERVER "sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean"
+
+# Check the exit status of the ssh command
+if [ $? -eq 0 ]; then
+  echo "Update Complete. ($SERVER)"
+else
+  echo "Update Failed! ($SERVER) - Check for errors above."
+fi
+sleep 5
+
+
